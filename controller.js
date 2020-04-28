bf99f6ec819dce126d39503e132f6c227e9c67fe
Zachary Allen Kravits <49537829+zakuxdesign@users.noreply.github.com>
2020-04-27 20:17:41 -0400

    Update controller.js

--- controller.js
+++ controller.js
@@ -2,22 +2,63 @@ import { View } from "./view.js";
 import { Model } from "./model.js";
 
 export class Controller {
+  //
+  //
+  //
+  //
+  //
+  //
+  // ------------------------------------------------------------------------------
+  // Checks Number Of Pieces
+  // ------------------------------------------------------------------------------
+  static Game_CountNumberOfConnectedPiece(
+    num,
+    whoToCheckFor,
+    checkingHumansPieces
+  ) {
+    //
+    // Goes through each of the scenarios (see "checks" array) and increments model.connectedPieces
+    model.data_ResetConnectedPieces();
+    console.log(`TURN ------------------ ${model.playerTurn}`);
+    const checks = [
+      model.data_CountPiecesInColumn(whoToCheckFor),
+      model.data_CountPiecesInRow(whoToCheckFor),
+      model.data_CountPiecesInIncreaseUpward(whoToCheckFor),
+      // model.data_CountPiecesInIncreaseDownward(whoToCheckFor),
+      // model.data_CountPiecesInDecreaseUpward(whoToCheckFor),
+      // model.data_CountPiecesInDecreaseDownward(whoToCheckFor),
+    ];
+    //
+    for (let check in checks) {
+      checks[check];
+      // if (model.connectedPieces <= num) {
+      //   console.log(model.connectedPieces);
+      // } else {
+      //   break;
+      // }
+    }
+  }
+
+  //
+  //
+  //
+  //
+  //
+  //
+  // ------------------------------------------------------------------------------
+  // Determines Who's Turn and what render and do
+  // ------------------------------------------------------------------------------
   Game_Turn() {
+    //
+    // Make the columns buttons
+    // so user can interact with them and make the columns clickable
     if (model.playerTurn == "human") {
-      //
-      // Make the columns buttons
-      // so user can interact with them
-      console.log(
-        model.playerTurn + "'s turn -----------------------------------"
-      );
-      console.log(model.turnNumber);
-      view.interface_InProgress("button", model.board);
+      view.interface_TurnInProgress("button", model.board);
       document.getElementById("paragraph-players-turn").innerHTML =
         "<u>Your</u> turn." +
         "</br>" +
         "Drop a piece by clicking one of the columns";
       //
-      // Make the columns clickable
       let columns = document.getElementsByClassName("column");
       for (let column of columns) {
         if (column.firstChild.className != "controlled-by-none") {
@@ -27,90 +68,60 @@ export class Controller {
           column.addEventListener("click", function () {
             let columnNumber = parseInt(column.getAttribute("data-col-number"));
             model.data_AddPieceToBoard(columnNumber);
-            Controller.Game_CountNumberOfConnectedPiece();
+            Controller.Game_CountNumberOfConnectedPiece(4, "human", false);
             view.interface_Remove();
-            if (model.connectedPieces == 4) {
-              Controller.Game_End();
-            } else {
-              controller.Game_ChangeTurn();
-            }
+            controller.Game_ChangeTurn();
+            // view.interface_Remove();
+            // console.log(model.connectedPieces);
+            // if (model.connectedPieces == 4) {
+            //   Controller.Game_End();
+            // } else {
+            //   controller.Game_ChangeTurn();
+            // }
           });
         }
       }
-    } else {
       //
-      // Make the columns divs
-      // so user cannot interact with the columns
-      console.log(
-        model.playerTurn + "'s turn -----------------------------------"
-      );
-      console.log(model.turnNumber);
-      view.interface_InProgress("div", model.board);
+      // Make the columns divs so user cannot interact with the columns
+    } else {
+      view.interface_TurnInProgress("div", model.board);
       document.getElementById("paragraph-players-turn").innerHTML =
         "Computer's turn." + "</br>" + "hmm...";
       //
-      // Computer decided where to put piece
-
-      //
-      // if it's first turn
-      // selects random column
+      // if first turn select random column
+      // if past computer's first turn looks to see if it needs to do a defensive move otherwise it does offensive move
+      // Checks if any scenario comes up
+      // if it gets through each scenario and nothing needs done it calls computerOffesiveMove()
       if (model.turnNumber == 2) {
         model.data_ComputerChoseFirstTurn();
-        //
-        // remove the UI
-        // if the connectPieces == 4 then end the game
-        // otherwise keep playing
         setTimeout(function () {
+          model.data_ResetConnectedPieces();
           view.interface_Remove();
-          if (model.connectedPieces == 4) {
-            Controller.Game_End();
-          } else {
-            controller.Game_ChangeTurn();
-          }
+          controller.Game_ChangeTurn();
         }, 2500);
-      }
-      //
-      // if it's past the first for the computer
-      // then it looks to see if it needs to do a defensive move
-      // otherwise it does offensive move
-      else {
+      } else {
         //
-        const checks = [
-          model.data_CountPiecesInColumn("human"),
-          model.data_CountPiecesInRow("human"),
-          model.data_CountPiecesInIncreaseUpward("human"),
-          model.data_CountPiecesInIncreaseDownward("human"),
-          model.data_CountPiecesInDecreaseUpward("human"),
-          model.data_CountPiecesInDecreaseDownward("human"),
-        ];
-
-        const computerOffensiveMove = () => {};
-
-        //
-        // Checks if any scenario comes up
-        // with at least > 2
-        // if it gets through each scenario and nothing needs done
-        // it calls computerOffesiveMove()
         const computerDefensiveMove = () => {
-          for (let check in checks) {
-            model.data_ResetConnectedPieces;
-            console.log(checks[check]);
-          }
-          setTimeout(function () {
-            view.interface_Remove();
-            if (model.connectedPieces == 4) {
-              Controller.Game_End();
-            } else {
-              controller.Game_ChangeTurn();
-            }
-          }, 2500);
+          Controller.Game_CountNumberOfConnectedPiece(2, "human", true);
         };
-        model.data_ResetConnectedPieces;
+        //
+        model.data_ResetConnectedPieces();
         computerDefensiveMove();
+        view.interface_Remove();
+        controller.Game_ChangeTurn();
       }
     }
   }
 
+  //
+  //
+  //
+  //
+  //
+  //
+  // ------------------------------------------------------------------------------
+  // Change Who's Turn It Is
+  // ------------------------------------------------------------------------------
   Game_ChangeTurn() {
     model.turnNumber += 1;
     model.data_ResetConnectedPieces();
@@ -123,40 +134,21 @@ export class Controller {
     }
   }
 
+  //
+  //
+  //
+  //
+  //
+  //
+  // ------------------------------------------------------------------------------
+  // Ends The Game
+  // ------------------------------------------------------------------------------
   static Game_End() {
     console.log(model.playerTurn + " wins");
     //
     //
     // new view for end game
     //
-  }
-
-  static Game_CountNumberOfConnectedPiece() {
-    model.data_ResetConnectedPieces();
-    model.data_CountPiecesInColumn(model.playerTurn);
-    if (model.connectedPieces < 4) {
-      model.data_ResetConnectedPieces();
-      model.data_CountPiecesInRow(model.playerTurn);
-    }
-    if (model.connectedPieces < 4) {
-      model.data_ResetConnectedPieces();
-      model.data_CountPiecesInIncreaseUpward(model.playerTurn);
-    }
-    if (model.connectedPieces < 4) {
-      model.data_ResetConnectedPieces();
-      model.data_CountPiecesInIncreaseDownward(model.playerTurn);
-    }
-    if (model.connectedPieces < 4) {
-      model.data_ResetConnectedPieces();
-      model.data_CountPiecesInDecreaseUpward(model.playerTurn);
-    }
-    if (model.connectedPieces < 4) {
-      model.data_ResetConnectedPieces();
-      model.data_CountPiecesInDecreaseDownward(model.playerTurn);
-    }
-    if (model.data_CountPieces < 4) {
-      model.data_ResetConnectedPieces();
-    }
   }
 }
 
